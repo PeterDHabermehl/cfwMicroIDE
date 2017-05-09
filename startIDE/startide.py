7c37f84db2371e1aff6fcb32f241f9d16433d40a
PeterDHabermehl <peter.habermehl@gmx.de>
2017-05-09 23:27:06 +0200

    this evening's work
    Signed-off-by: PeterDHabermehl <peter.habermehl@gmx.de>

--- startIDE/startide.py
+++ startIDE/startide.py
@@ -5,27 +5,67 @@ import sys
 from TouchStyle import *
 from TouchAuxiliary import *
 
+class QDblPushButton(QPushButton):
+    doubleClicked = pyqtSignal()
+    clicked = pyqtSignal()
+
+    def __init__(self, *args, **kwargs):
+        QPushButton.__init__(self, *args, **kwargs)
+        self.timer = QTimer()
+        self.timer.setSingleShot(True)
+        self.timer.timeout.connect(self.clicked.emit)
+        super().clicked.connect(self.checkDoubleClick)
+
+    @pyqtSlot()
+    def checkDoubleClick(self):
+        if self.timer.isActive():
+            self.doubleClicked.emit()
+            self.timer.stop()
+        else:
+            self.timer.start(250)
+
 class FtcGuiApplication(TouchApplication):
     def __init__(self, args):
         TouchApplication.__init__(self, args)
         
         #define variables for test
-        code=[ "# test",
-               "Output Rob 1 Set 7",
+        self.code=[ "# start",
+               "# test",
+               "Output Rob 1 7",
                "Delay 1000",
-               "Output Rob 1 Set 0",
+               "Output Rob 1 0",
+               "MotorPulse Rob 1 1 2 R 10",
+               "MotorEncod TXT 1 1 R 255",
+               "#",
                "# Ampel",
-               "Output TXT 1 Set 0",
-               "Output TXT 2 Set 0",
-               "Output TXT 3 Set 7",
+               "#",
+               "Tag gruen",
+               "# grün",
+               "Output TXT 1 0",
+               "Output TXT 2 0",
+               "Output TXT 3 512",
+               "Delay 2000",
+               "# grün - gelb",
+               "Output TXT 2 512",
                "Delay 1000",
-               "Output TXT 2 Set 7",
+               "# rot",
+               "Output TXT 1 512",
+               "Output TXT 2 0",
+               "Output TXT 3 0",
+               "Delay 2000",
+               "# gelb",
+               "Output TXT 1 0",
+               "Output TXT 2 512",
+               "Output TXT 3 0",
                "Delay 1000",
-               "Output TXT 1 Set 7",
-               "Output TXT 2 Set 0",
-               "Output TXT 3 Set 0"
+               "Jump gruen"
              ]
         
+        #self.code=["# head"]
+        
+        # would load presets here
+        
+        # open last project etc.
         
         # init internationalisation
         translator = QTranslator()
@@ -44,14 +84,17 @@ class FtcGuiApplication(TouchApplication):
 
         
         self.proglist=QListWidget()
-        self.proglist.setStyleSheet("font-family: 'Monospace'; font-size: 12px;")
+        self.proglist.setStyleSheet("font-family: 'Monospace'; font-size: 14px;")
+        self.proglist.itemDoubleClicked.connect(self.progItemDoubleClicked)
         
-        c=1
-        for a in code:
-            self.proglist.addItem(("  "+str(c))[-3:]+ ": "+a)
+        c=0
+        for a in self.code:
+            #self.proglist.addItem(("  "+str(c))[-3:]+ ": "+a)
+            self.proglist.addItem(a)
             c=c+1
             
         l.addWidget(self.proglist)
+        self.proglist.setCurrentRow(0)
         
         # and the controls
         
@@ -59,28 +102,28 @@ class FtcGuiApplication(TouchApplication):
         
         self.add = QPushButton("+")
         self.add.setStyleSheet("font-size: 20px;")
+        self.add.clicked.connect(self.addCodeLine)
         
-        self.rem = QPushButton("-")
+        self.rem = QDblPushButton("-")
         self.rem.setStyleSheet("font-size: 20px;")
+        self.rem.doubleClicked.connect(self.remCodeLine)
         
-        self.ins = QPushButton("I")
-        self.ins.setStyleSheet("font-size: 20px;")
-        
-        self.upp = QPushButton("Up")
+        self.upp = QPushButton("\u02C4") #QCoreApplication.translate("main","Up"))
         self.upp.setStyleSheet("font-size: 20px;")
+        self.upp.clicked.connect(self.lineUp)
         
-        self.don = QPushButton("Dn")
+        self.don = QPushButton("\u02C5")#QCoreApplication.translate("main","Dn"))
         self.don.setStyleSheet("font-size: 20px;")
+        self.don.clicked.connect(self.lineDown)
         
         h.addWidget(self.add)
         h.addWidget(self.rem)
-        h.addWidget(self.ins)
         h.addWidget(self.upp)
         h.addWidget(self.don)
         
         l.addLayout(h)
         
-        self.starter = QPushButton("Start")
+        self.starter = QPushButton(QCoreApplication.translate("main","Start"))
         self.starter.setStyleSheet("font-size: 20px;")
         
         l.addWidget(self.starter)
@@ -89,8 +132,39 @@ class FtcGuiApplication(TouchApplication):
         self.mainwindow.setCentralWidget(self.centralwidget)
         
         self.mainwindow.show()
-        self.exec_()        
+        self.exec_()
+    
+    def addCodeLine(self):
+        fta=TouchAuxMultibutton(QCoreApplication.translate("addcodeline","Add line"))
+        fta.setText(QCoreApplication.translate("addcodeline","Select command type:"))
+        fta.setButtons([ QCoreApplication.translate("addcodeline","Inputs"),
+                         QCoreApplication.translate("addcodeline","Outputs"),
+                         QCoreApplication.translate("addcodeline","Controls"),
+                         QCoreApplication.translate("addcodeline","Interaction")
+                        ]
+                      )
+        fta.setTextSize(2)
+        fta.setBtnTextSize(2)
+        (s,r)=fta.exec_()
+        if r==QCoreApplication.translate("addcodeline","Inputs"): print("inputs")
+        elif r==QCoreApplication.translate("addcodeline","Outputs"): print("outputs")
+        elif r==QCoreApplication.translate("addcodeline","Controls"):print("controls")
+        elif r==QCoreApplication.translate("addcodeline","Interaction"):print("interaction")
+                         
+    def remCodeLine(self):
+        print("remCodeLine")
+    def lineUp(self):
+        pass
+    def lineDown(self):
+        pass
 
+    def progItemDoubleClicked(self):
+        print("DOBLGLIG")
+        itm=self.proglist.currentItem()
+        row=self.proglist.currentRow()
+        cod=self.code[row]
+        print(itm.text(), row, ":", "'"+cod+"'")
+        
     def mkItemWidget(self, text):
         k=QHBoxLayout()
         s=""
